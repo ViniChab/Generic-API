9137cdfc5b2ad11b0c393639accabc121f577651
Vinícius Chab <vchab@gatefy.com>
2020-06-08 21:45:37 -0300

    Removing node_modules, adding first GET, adding .gitignore

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Generic-API
 A generic API made with Node.js
+
+# Why?
+This API is built so everytime you need a quick and simple Node.js API, you can just fork this and customize it
+
+# What is it built with?
+
+Knex and express, mostly. You can check the other packages in the package.json file, but its mostly really simples.
+
+# How do I start it?
+
+Just clone it, and run:
+```
+  npm i
+  npm start
+```
--- src/app/app.js
+++ src/app/app.js
@@ -1,14 +1,44 @@
-const express = require("express")
-const app = express()
+const express = require('express');
+const morgan = require('morgan');
+const mysql = require('mysql');
 
-app.get("/", (req, res) => {
-  res.send("Hello world")
-})
+const app = express();
+const mysqlConnection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'generic-schema',
+});
 
-app.get("/users", (req, res) => {
-  res.send("GET users")
-})
+app.use(morgan('short'));
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+app.get('/users', (req, res) => {
+  const users = [
+    {
+      firstName: 'Vinícius',
+      lastName: 'Chab',
+    },
+    {
+      firstName: 'Caroline',
+      lastName: 'Raiser',
+    },
+  ];
+  res.json(users);
+});
+
+app.get('/user/:id', (req, res) => {
+  mysqlConnection.query(
+    `SELECT * FROM users WHERE id = ${req.params.id}`,
+    (err, rows, fields) => {
+      res.json(rows);
+    }
+  );
+});
 
 app.listen(1600, () => {
-  console.log("API started")
-})
+  console.log('API started');
+});
