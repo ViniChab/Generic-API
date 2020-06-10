7498871f61d6998102658458eec166a634352fbb
Vinícius Chab <vchab@gatefy.com>
2020-06-09 23:08:43 -0300

    Adding homepage

--- src/app/app.js
+++ src/app/app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = require('./http/users.js');
+const path = require('path');
+const favicon = require('serve-favicon');
 
 const app = express();
 app.use(router);
 app.use(express.static('src/pages'));
+app.use(favicon('src/pages/assets/favicon.ico'));
 
 app.get('/', (req, res) => {
-  res.send('Hello world');
+  res.sendFile(path.join(__dirname, '../pages', 'main.html'));
 });
-
 
 app.listen(1600, () => {
   console.log('API restarted');
